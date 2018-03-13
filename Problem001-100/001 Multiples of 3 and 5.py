5d626402733cf8e2a002fb4b4993f7e439737ec7
XinguoAn <36292421+Anfany@users.noreply.github.com>
2018-03-13 16:22:53 +0800

    Add files via upload
    001 code file

---
+++ Problem001-100/001 Multiples of 3 and 5.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.5
+# -*- coding: UTF-8 -*-
+#Author: AnFany
+
+
+# Problem001 Multiples of 3 and 5
+
+an=sum([i for i in range(1,1000) if i%3==0 or i%5==0])
+print(an)
+'''
+´ð°¸£º233168
+'''
